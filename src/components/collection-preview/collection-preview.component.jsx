638d27684818f3af08723729c60588a48f18e865
oansky <oana.anghelachi@gmail.com>
2022-02-15 15:22:49 +0100

    Add shop page routing, map over preview component

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "./collection-preview.style.scss"
+
+const CollectionPreview = ({title, items}) => (
+	<div className='collection-preview'>
+		<h1>{title}</h1>
+		<div className='preview'>
+			{items.map(item => (
+				<div key={item.id}>{item.name}</div>
+			))}
+		</div>
+	</div>
+)
+
+export default CollectionPreview
